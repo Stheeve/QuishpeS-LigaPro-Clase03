364ffa150e5cb52ef37b390c8fc3c371001e0a02
Stheeven Quishpe <stheeven.quishpe@udla.edu.ec>
2025-04-10 07:20:57 -0500

    Agregar archivos de proyecto.

---
+++ QuishpeS-LigaPro-Clase03/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace QuishpeS_LigaPro_Clase03.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
